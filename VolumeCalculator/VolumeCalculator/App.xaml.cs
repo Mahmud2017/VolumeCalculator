f7418dfe851396851bee6679ea6242fb66e59c38
MAHMUD-PC\Mahmud <mhos@tu-chemnitz.de>
2019-09-09 22:04:15 +0200

    added startup codes on app.xaml.cs file

--- VolumeCalculator/VolumeCalculator/App.xaml.cs
+++ VolumeCalculator/VolumeCalculator/App.xaml.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using VolumeCalculator.Interfaces;
+using VolumeCalculator.Misc;
+using VolumeCalculator.Model;
+using VolumeCalculator.ViewModel;
 
 namespace VolumeCalculator
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// This is the application.
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Called when the applicaiton starts.
+        /// </summary>
+        /// <param name="e">Argumanets for the startup</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //Created all the interface objects
+            IReadFile irf = new ReadFile();
+            IVolumeCalculationHelper ivch = new VolumeCalculationHelper();
+            IVolumeCalculatorModel ivcm = new VolumeCalculatorModel();
+
+            //Created the view model object and passed the interfaces in the constructor
+            VolumeCalculatorVM vm = new VolumeCalculatorVM(ivch, ivcm, irf);
+
+            //Created the main window object
+            MainWindow mw = new VolumeCalculator.MainWindow();
+
+            //Added the view model object as Data Context of the main window
+            mw.DataContext = vm;
+
+            //Show the main window
+            mw.Show();
+        }
     }
 }
